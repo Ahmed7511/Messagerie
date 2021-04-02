0dc804e2f80eb027b2bbdb41ba89df916c12e1ad
Ahmed <sidouparadox7@gmail.com>
2021-04-02 15:28:10 +0200

    button déconnexion

--- View/homeView.php
+++ View/homeView.php
@@ -8,14 +8,24 @@
     <title>Document</title>
 </head>
 <body>
-
-   
+<nav class="nav justify-content-end">
+  
+    <a class="nav-link active" aria-current="page" <?php session_destroy(); ?> href="login.php">Déconnexion</a>
+  
+</nav>
 <form methode="POST" class="mb-3 px-3">
   <label for="formGroupExampleInput2" class="form-label"></label>
   <input type="text" class="form-control" name="content" id="formGroupExampleInput2" placeholder="entrer votre message ici ">
 </br>
-<input type="submit" value="envoyer" >
+  <div class="text-center">
+                <input type="submit" value="envoyer">
+  </div>
 </form>
 
-</body>
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script></body>
 </html>
